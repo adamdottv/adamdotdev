d88139a8c4bbb755c2448f0f1af7a350ae480935
adamdottv <2363879+adamdottv@users.noreply.github.com>
2024-07-12 10:45:22 -0500

    fix: secrets

--- infra/secret.ts
+++ infra/secret.ts
@@ -23,24 +23,15 @@ export const secret = {
     process.env.STRIPE_API_KEY,
   ),
   Twitch: {
-    ClientId: new sst.Secret("TwitchClientId", process.env.TWITCH_CLIENT_ID),
-    ClientSecret: new sst.Secret(
-      "TwitchClientSecret",
-      process.env.TWITCH_CLIENT_SECRET,
-    ),
-    UserId: new sst.Secret("TwitchUserId", process.env.TWITCH_USER_ID),
+    ClientId: new sst.Secret("TwitchClientId"),
+    ClientSecret: new sst.Secret("TwitchClientSecret"),
+    UserId: new sst.Secret("TwitchUserId"),
     Tokens: new aws.secretsmanager.Secret("TwitchTokens"),
   },
   Spotify: {
-    ClientId: new sst.Secret("SpotifyClientId", process.env.SPOTIFY_CLIENT_ID),
-    ClientSecret: new sst.Secret(
-      "SpotifyClientSecret",
-      process.env.SPOTIFY_CLIENT_SECRET,
-    ),
-    RefreshToken: new sst.Secret(
-      "SpotifyRefreshToken",
-      process.env.SPOTIFY_REFRESH_TOKEN,
-    ),
+    ClientId: new sst.Secret("SpotifyClientId"),
+    ClientSecret: new sst.Secret("SpotifyClientSecret"),
+    RefreshToken: new sst.Secret("SpotifyRefreshToken"),
     AccessToken: new aws.secretsmanager.Secret("SpotifyAccessToken"),
   },
 };
