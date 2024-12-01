38afba0b8f4b6af75517011c5c45a48269fdcdc1
adamdottv <2363879+adamdottv@users.noreply.github.com>
2024-11-30 20:31:24 -0600

    fix: mqtt

--- packages/web/src/hooks/use-topic.ts
+++ packages/web/src/hooks/use-topic.ts
@@ -1,57 +1,52 @@
-import { iot, mqtt } from "aws-iot-device-sdk-v2";
 import React from "react";
+import mqtt from "mqtt";
 
 const endpoint = process.env.NEXT_PUBLIC_REALTIME_ENDPOINT as string;
 const authorizer = process.env.NEXT_PUBLIC_REALTIME_AUTHORIZER as string;
 const topic = process.env.NEXT_PUBLIC_REALTIME_TOPIC as string;
 
-async function createConnection(endpoint: string, authorizer: string) {
-  return new Promise<mqtt.MqttClientConnection>((resolve, reject) => {
-    const client = new mqtt.MqttClient();
-    const id = window.crypto.randomUUID();
-    const connection = client.new_connection(
-      iot.AwsIotMqttConnectionConfigBuilder.new_with_websockets()
-        .with_clean_session(true)
-        .with_client_id(`client_${id}`)
-        .with_endpoint(endpoint)
-        .with_keep_alive_seconds(30)
-        .with_custom_authorizer("", authorizer, "", "PLACEHOLDER_TOKEN")
-        .build(),
-    );
-    connection.on("error", (error) => {
-      console.error(error);
-      reject(error);
-    });
-    connection.on("connect", (_sessionPresent) => {
-      resolve(connection);
-    });
-    connection.connect();
-  });
+function createConnection(endpoint: string, authorizer: string) {
+  return mqtt.connect(
+    `wss://${endpoint}/mqtt?x-amz-customauthorizer-name=${authorizer}`,
+    {
+      protocolVersion: 5,
+      manualConnect: true,
+      username: "", // Must be empty for the authorizer
+      password: "PLACEHOLDER_TOKEN", // Passed as the token to the authorizer
+      clientId: `client_${window.crypto.randomUUID()}`,
+    },
+  );
 }
 
-const subscribers: Map<string, { connection: mqtt.MqttClientConnection }> =
-  new Map();
+const subscribers: Map<string, { connection: mqtt.MqttClient }> = new Map();
 export const useTopic = <T>(key: string, onMessage: (message: T) => void) => {
   React.useEffect(() => {
     if (subscribers.has(key)) return;
 
-    createConnection(endpoint, authorizer)
-      .then((conn) => {
-        if (subscribers.has(key)) return;
-        subscribers.set(key, { connection: conn });
+    const connection = createConnection(endpoint, authorizer);
 
-        conn.subscribe(topic, mqtt.QoS.AtLeastOnce, (_topic, payload) => {
-          const raw = new TextDecoder("utf8").decode(new Uint8Array(payload));
-          const message = JSON.parse(raw) as T;
-          onMessage(message);
-        });
-      })
-      .catch(console.error);
+    connection.on("connect", async () => {
+      if (subscribers.has(key)) return;
+      subscribers.set(key, { connection });
+
+      try {
+        await connection.subscribeAsync(topic, { qos: 1 });
+      } catch (e) {}
+    });
+
+    connection.on("message", (_fullTopic, payload) => {
+      const raw = new TextDecoder("utf8").decode(new Uint8Array(payload));
+      const message = JSON.parse(raw) as T;
+      onMessage(message);
+    });
+    connection.on("error", console.error);
+    connection.connect();
 
     return () => {
       if (subscribers.has(key)) {
         const { connection } = subscribers.get(key)!;
         connection.unsubscribe(topic);
+        connection.end();
         subscribers.delete(key);
       }
     };
